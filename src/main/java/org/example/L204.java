2efe674338b7283e7614db350d24ce51d1a7a2ae
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-10-11 08:42:39 +0330

    215

--- src/main/java/org/example/L204.java
+++ src/main/java/org/example/L204.java
@@ -1,5 +1,24 @@
 package org.example;
 
 class L204 {
-
+    //https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    public int countPrimes(int n) {
+        int res = 0;
+        var used = new boolean[n];
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (!used[i - 1]) {
+                int temp = i * i;
+                while (temp < n) {
+                    used[temp - 1] = true;
+                    temp += i;
+                }
+            }
+        }
+        for (int i = 2; i < n; i++) {
+            if (!used[i - 1]) {
+                res++;
+            }
+        }
+        return res;
+    }
 }
--- src/test/java/org/example/L204Test.java
+++ src/test/java/org/example/L204Test.java
@@ -1,11 +1,19 @@
 package org.example;
 
+
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 
+class L204Test {
+    @Test
+    public void countPrimes() {
+        L204 countPrimes = new L204();
 
-class L0Test {
+        assertEquals(4, countPrimes.countPrimes(10));
+        assertEquals(0, countPrimes.countPrimes(1));
+        assertEquals(0, countPrimes.countPrimes(2));
+        assertEquals(1, countPrimes.countPrimes(3));
 
+    }
 }
